a63b1aaf6a2438dc072f5561b97cfb49eaea3c4d
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2023-03-06 20:22:20 +0100

    feat: add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lets_meet/src/app.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp();
+
   runApp(const App());
 }
